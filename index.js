d860e1b21741e04e0019f760d91f55508b5c5a79
TristanKhadka11 <148723177+freekhadka@users.noreply.github.com>
2024-10-19 21:54:19 -0400

    v16

--- index.js
+++ index.js
@@ -160,3 +160,14 @@ sections.forEach((sec) => {
             }
         });
     });
+
+
+    const headings = document.querySelectorAll('.heading');
+    window.addEventListener('scroll', () => {
+        const scrollValue = window.scrollY;
+        headings.forEach((heading, index) => {
+            heading.style.top = `${scrollValue * .066 * index}px`;
+        });
+    });
+    
+    
